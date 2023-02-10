732b4ae2fb6b767bcfe84cf55a690a75e62d6586
Leiladany <leiladany16@gmail.com>
2023-02-10 12:12:02 +0000

    ?

--- Pawsitive-Pets/Pawsitive-Pets/models/Info.model.js
+++ Pawsitive-Pets/Pawsitive-Pets/models/Info.model.js
@@ -3,23 +3,39 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
+    petname: {
       type: String,
       trim: true,
       required: false,
       unique: true
     },
-    email: {
+    petsort: {
+        type: String,
+        required: true
+    },
+    petbreed: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
       trim: true
     },
-    password: {
+    petbirth: {
       type: String,
       required: true
-    }
+    },
+    petcolor: {
+        type: String,
+        required: true
+    },
+    petvaccines: {
+        type: String,
+        required: true
+    },
+    petpicture: {
+        type: String,
+        required: true
+    },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`    
