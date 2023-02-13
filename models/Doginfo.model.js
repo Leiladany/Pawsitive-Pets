22ee8122634462dc3dbcebefbc96ee87d287cf59
Leiladany <leiladany16@gmail.com>
2023-02-13 14:52:23 +0000

    Change doginfo.model

--- models/Doginfo.model.js
+++ models/Doginfo.model.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
-const userSchema = new Schema(
+const dogSchema = new Schema(
   {
     dogamount: {
       type: Number,
@@ -57,6 +57,6 @@ const userSchema = new Schema(
   }
 );
 
-const User = model("User", userSchema);
+const Dog = model("Dog", userSchema);
 
-module.exports = User;
+module.exports = Dog;
