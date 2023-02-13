d3705a8c894e8701f374e1be0c792f485f5372b7
Leiladany <leiladany16@gmail.com>
2023-02-13 15:07:32 +0000

    update cat info model

--- models/Catinfo.model.js
+++ models/Catinfo.model.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
-const userSchema = new Schema(
+const catSchema = new Schema(
   {
     catamount: {
       type: Number,
@@ -56,6 +56,6 @@ const userSchema = new Schema(
   }
 );
 
-const User = model("User", userSchema);
+const Cat = model("Cat", catSchema);
 
-module.exports = User;
+module.exports = Cat;
