77827e7b953a9e9340f0f896069c754a1d7756ee
Miguel Barroca <miguelbarroca@protonmail.com>
2023-02-14 16:35:54 +0000

    find (all) method working

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -9,15 +9,13 @@ router.get('/', isLoggedIn, (req, res) => {
     res.render('profile', { user: req.session.user })
   })
 
-  router.get('/create', (req, res) => {
-    res.render('animals/create')
+  router.get('/addpet', (req, res) => {
+    res.render('animals/addpet')
 
   })
-
-  router.post('/create', async (req, res, next) => {
-    const body = req.body
-    console.log(req.body, "🐶🐶🐶🐶🐶🐶🐶🐶🐶🐶🐶🐶🐶🐶")
-    
+//create a new pet
+  router.post('/addpet', async (req, res, next) => {
+    const body = req.body    
     const createPet = await Pet.create({
       petname: body.petname,
       petsort: body.petsort,
@@ -34,8 +32,18 @@ router.get('/', isLoggedIn, (req, res) => {
     res.redirect('/profile')
   })
 
+  router.get('/mypets', async (req, res) => {
+    try {
+      const allPets = await Pet.find()
+      res.render('animals/mypets', { hopper: allPets })
+    } catch (error) {
+      console.log('Route to all recipes', error)
+    }
+  })
+  
+
 //? Pet.create({...req.body, owner: currentUser._id})
 //? Pet.find({owner: currentUser._id})
 
 
-  module.exports = router 
+  module.exports = router
