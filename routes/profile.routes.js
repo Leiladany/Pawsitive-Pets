7571cbd0cbbcc04afcff94ac5596202e22b7eb85
Miguel Barroca <miguelbarroca@protonmail.com>
2023-02-14 11:06:54 +0000

    changes to profile.routes & animals/create

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,30 +1,35 @@
 const express = require('express')
 const { isLoggedIn } = require('../middleware/route-guard')
 const router = express.Router()
+const Pet = require('../models/Pet.model')
+const User = require('../models/User.model')
+
 
 router.get('/', isLoggedIn, (req, res) => {
-    
     res.render('profile', { user: req.session.user })
   })
 
-  router.get('/profile/new', async (req, res, next) => {
-    res.render('/profile', { update: false })
+  router.get('/create', async (req, res, next) => {
+    const body = req.body
+    const createPet = await Pet.create({
+      ...body,
+      petamount: body.petamount,
+      petname: body.petname,
+      petsort: body.petsort,
+      petbreed: body.petbreed,
+      petbirth: body.petbirth,
+      petsex: body.petsex,
+      petcolor: body.petcolor,
+      pethair: body.pethair,
+      petvaccines: body.petvaccines
+      //missing petpicture
+    })
+    res.redirect('/recipes')
   })
-  
+
 
 //! Pet.create({...req.body, owner: currentUser._id})
 //!Pet.find({owner: currentUser._id})
-
-
-
-
-
-
-
-
-
-
-
 
 
   module.exports = router 
